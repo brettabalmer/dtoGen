4da4ced4e7aa5c07828b7c29ebb13cfb3b97d7f4
Brett Balmer <brettabalmer@hotmail.com>
2025-06-05 15:34:57 -0400

    Typescript code gen using Nswag

---
+++ Program.cs
@@ -0,0 +1,58 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+//builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(settings =>
+{
+    settings.Title = "Minimal API";
+    settings.Version = "v1";
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    //app.MapOpenApi();
+
+    app.UseOpenApi();
+    app.UseSwaggerUi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+Task.Run(async () =>
+{
+    Thread.Sleep(5000); // wait for the server to start
+    await new TsClientGenerator().SimpleGenerate( "http://localhost:5016/swagger/v1/swagger.json");
+});
+
+app.Run();
+
+
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
